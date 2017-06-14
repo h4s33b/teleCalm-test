de58a25b5dd5505aaa9797f6953f6283cf95f915
unknown <haseeb.rehman@iunc.edu.pk>
2017-06-14 13:55:02 +0500

    audio recording

--- www/js/app.js
+++ www/js/app.js
@@ -4,7 +4,7 @@
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 // 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.controllers'])
+angular.module('starter', ['ionic', 'starter.controllers' , 'angularAudioRecorder'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -22,7 +22,14 @@ angular.module('starter', ['ionic', 'starter.controllers'])
   });
 })
 
-.config(function($stateProvider, $urlRouterProvider) {
+.config(function($stateProvider, $urlRouterProvider ,recorderServiceProvider) {
+
+   recorderServiceProvider
+      .forceSwf(false)
+      //.setSwfUrl('/lib/recorder.swf')
+      .withMp3Conversion(true)
+    ;
+
   $stateProvider
 
     .state('app', {
